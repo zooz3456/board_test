014eefe4c2fdb686f965cd40e54b6ffab05e0f90
gns12 <31301504+gns12@users.noreply.github.com>
2017-09-06 15:21:14 +0900

    Update board_join_proc.php

--- board_join_proc.php
+++ board_join_proc.php
@@ -23,8 +23,7 @@ $id=mysqli_real_escape_string($conn,$_POST['id']);
 $pass=mysqli_real_escape_string($conn,$_POST['pass']);
 $name=mysqli_real_escape_string($conn,$_POST['name']);
 $email=mysqli_real_escape_string($conn,$_POST['email']); 
-$sql="insert into user(id,pass,name,email)
-	values('{$id}',sha2('{$pass}',0),'{$name}','{$email}')";
+$sql="insert into user(id,pass,name,email) values('{$id}',sha2('{$pass}',0),'{$name}','{$email}')";
 $result=mysqli_query($conn,$sql);
 
 if($result)
