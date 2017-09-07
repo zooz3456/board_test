ceb629925b378014fc8394de018456e35d3d1d16
gns12 <31301504+gns12@users.noreply.github.com>
2017-09-07 15:51:57 +0900

    수정
    1.mysql구문을 사용 했을 경우 mysql close()구문이 있어야 되는데 myslq close()구문이 있음
    
    2.로그인 안한 상태에서 수정 페이지로 접근 가능 여부 확인 해야 되는데 페이지가 접속이 안되고 페이지에 php오류 문구가 출력됨

--- rmftn.php
+++ rmftn.php
@@ -28,15 +28,24 @@ input.button{
 </style> 
 </head> 
 <body> 
+<?php
+SESSION_START();
+?>
 <?php 
-$no=$_GET['no']; 
-include 'conn.php'; 
+if(!isset($_SESSION['id']))
+{
+  exit;
+} 
 
-$sql="select * from board1_img where no={$no}"; 
+$no=$_GET['no']; 
+
+$sql="select * from board1 where no={$no}"; 
 $result=mysqli_query($conn,$sql); 
 $arr=mysqli_fetch_assoc($result); 
 mysqli_close($conn); 
+
 ?> 
+
 <div align='center'> 
 <table class='type06' border="1" width="50%" align='center'> 
 <form method="POST" action="tnwjd.php"> 
@@ -48,15 +57,20 @@ value='<?=$arr['subject']?>'></td></tr>
 
 <tr><th class='type06' width="90px">첨부파일</th><td> 
 
-<tr><td class='type06' colspan="2">본문 내용</td></tr> 
-
+<tr><td class='type06' colspan="2">본문 내용</td></tr>
+<?php include 'conn.php'; ?>
+<?php
+if(!isset($_SESSION['id']))
+{
+  exit;
+} 
+?>
 <tr><td colspan="2"><textarea cols='80' rows='7' name='content'><?=$arr['content']?> 
 </textarea></td></tr> 
 <tr><th class='type06' colspan='2'><input type='submit' name='mod' value='수정하기'></th></tr> 
 </form> 
 </table> 
 </div> 
-
 
 </body> 
 
